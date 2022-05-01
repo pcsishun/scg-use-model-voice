34796d5356f597a149f304544311d6cc79201a0a
pcsishun <pcsishun@gmail.com>
2022-05-01 13:36:33 +0700

    update alway open audio

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import recorder
 import pvporcupine
 import pyaudio
 import struct
+import os
+
 # from tensorflow import keras 
 
 filename="test.wav"
@@ -87,44 +89,51 @@ class livePredictions:
 #from the RAVDESS dataset you want to use for the prediction,
 # Below, I have used a neutral file: the prediction made is neutral.
 
-try:
-    print("start app...")
-    accesskey = "4QUFReTvGjJJnCxTrw7JPgATaHClIGekXV/cuJzYYD6cO3K4fs3qMA=="
-    porcupine = pvporcupine.create(access_key=accesskey,keywords=["computer", "alexa"])
-    pa = pyaudio.PyAudio()
-    audio_stream = pa.open(
-        rate=porcupine.sample_rate,
-        channels=1,
-        format=pyaudio.paInt16,
-        input=True,
-        frames_per_buffer=porcupine.frame_length)
-    
-    while True:
-        pcm = audio_stream.read(porcupine.frame_length)
-        pcms = struct.unpack_from("h" * porcupine.frame_length, pcm)
-        keyword_index = porcupine.process(pcms)
-        print(keyword_index)
-
-        if keyword_index >= 0:
-            print("startRecord")
-            recorder.record(filename)
-            pred = livePredictions(path='SER_model.h5',file='test.wav')
-
-            pred.load_model()
-            pred.makepredictions()
-            break
-            
-except:
-    print("error detect!")
-    if porcupine is not None:
-        porcupine.delete()
-        print("End")
-    if audio_stream is not None:
-        audio_stream.close()
-        print("End")
-    if pa is not None:
-        pa.terminate()
-        print("End")
-
+def app_start():
+    try:
+        print("start app...")
+        accesskey = "4QUFReTvGjJJnCxTrw7JPgATaHClIGekXV/cuJzYYD6cO3K4fs3qMA=="
+        porcupine = pvporcupine.create(access_key=accesskey,keywords=["computer", "alexa"])
+        pa = pyaudio.PyAudio()
+        audio_stream = pa.open(
+            rate=porcupine.sample_rate,
+            channels=1,
+            format=pyaudio.paInt16,
+            input=True,
+            frames_per_buffer=porcupine.frame_length)
  
+        while True:
+            pcm = audio_stream.read(porcupine.frame_length)
+            pcms = struct.unpack_from("h" * porcupine.frame_length, pcm)
+            keyword_index = porcupine.process(pcms)
+            print(keyword_index)
 
+            if keyword_index >= 0:
+                print("startRecord")
+                recorder.record(filename)
+                pred = livePredictions(path='SER_model.h5',file='test.wav')
+
+                pred.load_model()
+                pred.makepredictions()
+                os.remove("test.wav")
+                print("finish predict")
+                break
+    
+                
+    except:
+        print("error detect!")
+        if porcupine is not None:
+            porcupine.delete()
+            print("End")
+        if audio_stream is not None:
+            audio_stream.close()
+            print("End")
+        if pa is not None:
+            pa.terminate()
+            print("End")
+
+
+action = True
+while action == True:
+    print("action..")
+    app_start()
